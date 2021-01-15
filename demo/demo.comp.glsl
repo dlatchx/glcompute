38792e6b7172acb745e5074c4d6a1478ed2d7976
Babz <babz@tfnux.org>
2021-01-15 08:05:19 +0100

    reformat code

--- demo/demo.comp.glsl
+++ demo/demo.comp.glsl
@@ -3,15 +3,15 @@
 #extension GL_ARB_shader_storage_buffer_object: enable
 
 layout(std430, binding=0) buffer Buf1 {
-	float foo[];
+  float foo[];
 };
 
 layout(
-	local_size_x = 256,
-	local_size_y = 1,
-	local_size_z = 1) in;
+       local_size_x = 256,
+       local_size_y = 1,
+       local_size_z = 1) in;
 
 void main() {
   uint i = gl_GlobalInvocationID.x;
-	foo[i] = sqrt(foo[i]);
+  foo[i] = sqrt(foo[i]);
 }
